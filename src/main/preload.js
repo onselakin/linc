2741e19ee1209a5714ea7f4d811e06fe9a2dd93d
onsel.akin <onsela@outlook.com>
2022-02-13 13:56:02 +0300

    Remove logging from preloader

--- src/main/preload.js
+++ src/main/preload.js
@@ -3,13 +3,11 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
   ipcRenderer: {
     on(channel, handler) {
-      console.log('Registering handler for channel: ', channel);
       ipcRenderer.on(channel, (_event, ...args) => {
         handler(args);
       });
     },
     send(channel, ...args) {
-      console.log(`Sending ${args} over channel ${channel}`);
       ipcRenderer.send(channel, args);
     },
   },
