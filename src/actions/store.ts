51e156d8fd8b2ed5cd69a3e3f16603d52f5f8af9
onsel.akin <onsela@outlook.com>
2022-02-19 22:19:06 +0300

    Refactored IPC

--- src/actions/store.ts
+++ src/actions/store.ts
@@ -1,29 +1,30 @@
-import { ActionRequest, ActionResponse } from 'ipc';
-
+import { Bridge } from 'ipc/ipc-handler';
 import { Settings } from 'types/settings';
 import store from '../store';
 
-const storeActions = {
-  'load-settings': async (_req: ActionRequest, res: ActionResponse<Settings>) => {
-    try {
-      res.reply(store.get('settings'));
-    } catch (error) {
-      res.error(error);
-    }
-  },
-  'save-settings': async (req: ActionRequest<{ settings: Settings }>, res: ActionResponse) => {
-    try {
-      const currentSettings = store.get('settings');
-      const merged = {
-        ...currentSettings,
-        ...req.payload.settings,
-      };
-      store.set('settings', merged);
-      res.reply();
-    } catch (error) {
-      res.error(error);
-    }
-  },
+const loadSettings: Bridge<never, Settings> = async (_payload, channel) => {
+  try {
+    channel.reply(store.get('settings'));
+  } catch (error) {
+    channel.error(error);
+  }
 };
 
-export default storeActions;
+const saveSettings: Bridge<Settings, void, unknown> = async (payload, channel) => {
+  try {
+    const currentSettings = store.get('settings');
+    const merged = {
+      ...currentSettings,
+      ...payload,
+    };
+    store.set('settings', merged);
+    channel.reply();
+  } catch (error) {
+    channel.error(error);
+  }
+};
+
+export default {
+  'load-settings': loadSettings,
+  'save-settings': saveSettings,
+};
