795aa0445fde59faa56828e0661d2cffaf13e738
onsel.akin <onsela@outlook.com>
2022-02-20 20:27:20 +0300

    Add an ugly button to the sidebar

--- src/renderer/components/App/SideBar.tsx
+++ src/renderer/components/App/SideBar.tsx
@@ -1,6 +1,15 @@
 import 'renderer/App.css';
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
-  return <div className="absolute top-14 left-0 w-14 bottom-0 border-2 border-blue-500 bg-green-500">ICO</div>;
+  return (
+    <div className="absolute top-14 left-0 w-14 bottom-0 border-2 flex content-center justify-center border-blue-500 bg-green-500">
+      <Link to="/">
+        <button type="button" className="w-6 h-6 bg-amber-100">
+          L
+        </button>
+      </Link>
+    </div>
+  );
 };
 export default SideBar;
