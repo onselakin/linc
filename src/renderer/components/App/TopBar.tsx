c505af1bf1549b3d168cb66e64bfc5336f80cf82
onsel.akin <onsela@outlook.com>
2022-02-20 18:24:21 +0300

    Fix TopBar dimensions

--- src/renderer/components/App/TopBar.tsx
+++ src/renderer/components/App/TopBar.tsx
@@ -1,6 +1,6 @@
 import 'renderer/App.css';
 
 const TopBar = () => {
-  return <div className="absolute left-0 w-14 top-0 h-14 w-full border-2 border-red-500 bg-blue-500">Top</div>;
+  return <div className="absolute left-0 top-0 right-0 h-14 w-full border-2 border-red-500 bg-blue-500">Top</div>;
 };
 export default TopBar;
