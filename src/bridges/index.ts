7cd03194ff640efbc009848f80cc30c2c989ec3c
onsel.akin <onsela@outlook.com>
2022-02-20 11:46:45 +0300

    Renamed settings bridge file name.

--- src/bridges/index.ts
+++ src/bridges/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import labActions from './labs';
-import storeActions from './store';
+import storeActions from './settings';
 
 const actions = { ...labActions, ...storeActions };
 export default actions;
