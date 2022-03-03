4a6784a2ffffaf82f6bbbbb30957e057cde09d43
onsel.akin <onsela@outlook.com>
2022-03-03 19:59:03 +0300

    Add content loading to labs bridge

--- src/bridges/labs.ts
+++ src/bridges/labs.ts
@@ -7,6 +7,8 @@ import fs from 'fs';
 import http from 'isomorphic-git/http/node';
 import path from 'path';
 import yaml from 'js-yaml';
+import Scenario from 'types/scenario';
+import Step from 'types/step';
 
 const labsPath = path.join(app.getPath('userData'), 'labwiz', 'labs');
 
@@ -54,16 +56,51 @@ const cloneLab: Bridge<
 };
 
 const loadLab: Bridge<{ name: string }, Lab, unknown> = async (payload, channel) => {
+  const labPath = path.join(labsPath, payload.name);
   const labFile = path.join(labsPath, payload.name, 'lab.yaml');
-  console.log(labFile);
   if (!fs.existsSync(labFile)) {
     channel.error(new Error('Lab file not found.'));
     return;
   }
   // TODO: Add schema validation
   try {
-    const labYaml = yaml.load(fs.readFileSync(labFile, 'utf-8')) as Lab;
-    channel.reply(labYaml);
+    const lab = yaml.load(fs.readFileSync(labFile, 'utf-8')) as Lab;
+    const coverFile = path.join(labPath, 'cover.md');
+    if (fs.existsSync(coverFile)) {
+      lab.cover = fs.readFileSync(coverFile, 'utf-8');
+    }
+    const scenariosPath = path.join(labPath, 'scenarios');
+    // Walk through the folders and add the scenarios
+    lab.scenarios = fs
+      .readdirSync(scenariosPath, { withFileTypes: true })
+      .filter(dir => dir.isDirectory())
+      .map(dir => {
+        const scenarioFile = path.join(scenariosPath, dir.name, 'scenario.yaml');
+        const scenario = yaml.load(fs.readFileSync(scenarioFile, 'utf-8')) as Scenario;
+        const stepsPath = path.join(scenariosPath, dir.name, 'steps');
+        if (!fs.existsSync(stepsPath)) {
+          scenario.steps = [];
+          return scenario;
+        }
+        scenario.steps = fs
+          .readdirSync(stepsPath, { withFileTypes: true })
+          .filter(stepDir => stepDir.isDirectory())
+          .sort((a, b) => (a.name < b.name ? -1 : 1))
+          .map(stepDir => {
+            const stepPath = path.join(scenariosPath, dir.name, 'steps', stepDir.name);
+            const step = yaml.load(fs.readFileSync(path.join(stepPath, 'step.yaml'), 'utf-8')) as Step;
+            const contentFilePath = path.join(stepPath, 'content.md');
+            if (fs.existsSync(contentFilePath)) {
+              step.content = fs.readFileSync(contentFilePath, 'utf-8');
+            }
+            return step;
+          });
+        return scenario;
+      });
+
+    console.log(lab);
+
+    channel.reply(lab);
   } catch (e) {
     console.log(e);
     channel.error(e);
