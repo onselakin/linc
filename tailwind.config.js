eb8ddf0e874797d6f17545eee438f407da32e3b4
onsel.akin <onsela@outlook.com>
2022-03-03 19:58:43 +0300

    Add styles for markdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,39 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,ejs}'],
   theme: {
+    fontFamily: {
+      mono: ['Ubuntu Mono'],
+    },
     extend: {
       colors: {
         container: '#2B2F42',
         component: '#262739',
         icon: '#788AB6',
         green: '#02FBA2',
+        orange: '#FD9900',
       },
       typography: {
         DEFAULT: {
           css: {
+            color: '#bbb',
+            h1: {
+              color: '#788AB6',
+            },
+            h2: {
+              color: '#788AB6',
+            },
+            h3: {
+              color: '#788AB6',
+            },
+            blockquote: {
+              color: '#bbb',
+            },
             pre: {
               margin: '0',
               'border-top-left-radius': 0,
               'border-top-right-radius': 0,
+              'background-color': '#2B2F42',
+              'border-radius': '5px',
             },
           },
         },
