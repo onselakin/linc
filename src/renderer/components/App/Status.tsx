934cd95832bd6bb3ce9f8f1126fb42e417238877
onsel.akin <onsela@outlook.com>
2022-03-03 20:31:31 +0300

    Updated status bar design

--- src/renderer/components/App/Status.tsx
+++ src/renderer/components/App/Status.tsx
@@ -7,11 +7,18 @@ const Status = () => {
   const status = useRecoilValue(statusAtom);
 
   return (
-    <div className="absolute left-0 bottom-0 right-0 h-10 bg-component flex">
-      <div className="flex-1">{status.message}</div>
+    <div className="absolute left-0 bottom-0 right-0 h-10 bg-container flex">
+      <div className="flex-1 flex items-center pl-4 text-gray-400 gap-2">
+        {status.message !== '' && (
+          <>
+            <i className="fa-solid fa-circle-info fa-md" />
+            <span>{status.message}</span>
+          </>
+        )}
+      </div>
       <div className="mr-4 mt-1 font-mono text-gray-400">
         <div className="flex rounded-xl h-8 px-4 bg-container items-center gap-2">
-          Connected to Docker <i className={`fa-solid fa-plug fa-md text-green`} />
+          Not Connected to Docker <i className="fa-solid fa-plug fa-md text-red-500" />
         </div>
       </div>
     </div>
