00da16cf2a1a898a9c5e12d6a0f95f308f701515
onsel.akin <onsela@outlook.com>
2022-03-05 15:29:05 +0300

    Fixed problems with state management that crashed terminals

--- src/renderer/components/Terminal.tsx
+++ src/renderer/components/Terminal.tsx
@@ -1,16 +1,41 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
 import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { CreateChannel } from '../../ipc/ipc-emitter';
 
-const Term = ({ value }: { value: number }) => {
+interface TerminalProps {
+  size: number;
+  visible: boolean;
+}
+
+export interface TerminalRef {
+  exit: () => void;
+  fit: () => void;
+}
+
+const createTerminal = () => {
+  console.log('Creating a terminal');
+  return new Terminal({ cols: 80, rows: 25, fontFamily: 'Ubuntu Mono', fontSize: 16 });
+};
+
+const Term = forwardRef<TerminalRef, TerminalProps>(({ size, visible }, ref) => {
+  const exitCallRef = useRef<() => void>();
   const fit = useRef<FitAddon>(new FitAddon());
   const xtermContainer = useRef<HTMLDivElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    exit() {
+      exitCallRef.current?.();
+    },
+    fit() {
+      fit.current.fit();
+    },
+  }));
 
   useEffect(() => {
     if (xtermContainer.current !== null) {
       const terminalId = Math.random().toString(36).slice(-5);
-      const term = new Terminal({ cols: 80, rows: 25, fontFamily: 'Ubuntu Mono', fontSize: 16 });
+      const term = createTerminal();
       term.loadAddon(fit.current);
       term.open(xtermContainer.current);
       fit.current.fit();
@@ -24,14 +49,22 @@ const Term = ({ value }: { value: number }) => {
       term.onData(data => {
         channel.send({ terminalId, command: data });
       });
+      exitCallRef.current = () => {
+        channel.send({ terminalId, command: 'exit' });
+      };
     }
   }, []);
 
   useEffect(() => {
     fit.current.fit();
-  }, [value]);
+  }, [size]);
 
-  return <div className="h-full" ref={xtermContainer} />;
-};
+  return (
+    <div
+      className={`absolute top-0 right-0 bottom-0 left-0 ${visible ? 'visible' : 'invisible'}`}
+      ref={xtermContainer}
+    />
+  );
+});
 
 export default Term;
