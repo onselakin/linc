d8574595b82ec9272d9b12789892df93c7e52ef4
onsel.akin <onsela@outlook.com>
2022-03-06 11:26:37 +0300

    Establish connection to Docker engine

--- src/bridges/docker.ts
+++ src/bridges/docker.ts
@@ -1,10 +1,26 @@
 import { Bridge } from 'ipc/ipc-handler';
 
+const { Docker } = require('node-docker-api');
+
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
+const connect: Bridge<void, void, { success: boolean }> = async (_, channel) => {
+  setInterval(async () => {
+    try {
+      const reply = await docker.ping();
+      channel.notify({ success: reply === 'OK' });
+    } catch (error) {
+      channel.notify({ success: false });
+    }
+  }, 5000);
+};
+
 const pullImage: Bridge<{ image: string }, { success: boolean }> = async (payload, channel) => {
-  console.log(`Cloning repo: ${payload}`);
+  docker.image.console.log(`Cloning repo: ${payload}`);
   channel.reply({ success: true });
 };
 
 export default {
-  'pull-image': pullImage,
+  'docker:connect': connect,
+  'docker:pull-image': pullImage,
 };
--- src/bridges/index.ts
+++ src/bridges/index.ts
@@ -3,6 +3,7 @@
 import labActions from './labs';
 import storeActions from './settings';
 import terminalActions from './terminal';
+import dockerActions from './docker';
 
-const actions = { ...labActions, ...storeActions, ...terminalActions };
+const actions = { ...labActions, ...storeActions, ...terminalActions, ...dockerActions };
 export default actions;
