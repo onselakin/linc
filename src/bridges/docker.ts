709217ea649de61d199d82c272c7c9a38b037135
onsel.akin <onsela@outlook.com>
2022-03-07 00:44:13 +0300

    Added image pull command to docker ipc handler

--- src/bridges/docker.ts
+++ src/bridges/docker.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Bridge } from 'ipc/ipc-handler';
 
 const { Docker } = require('node-docker-api');
@@ -15,12 +17,50 @@ const connect: Bridge<void, void, { success: boolean }> = async (_, channel) => 
   }, 5000);
 };
 
-const pullImage: Bridge<{ image: string }, { success: boolean }> = async (payload, channel) => {
-  docker.image.console.log(`Cloning repo: ${payload}`);
-  channel.reply({ success: true });
+const inspect: Bridge<{ imageName: string }, unknown> = async ({ imageName }, channel) => {
+  const images = await docker.image.list();
+  const image = images.find((i: any) => i.data.RepoTags.includes(imageName));
+  if (image === undefined) {
+    channel.error(new Error('IMAGE_NOT_FOUND'));
+    return;
+  }
+  const imageStatus = await image.status();
+  channel.reply(imageStatus.data);
+};
+
+const pull: Bridge<
+  { image: string; tag: string },
+  void,
+  { status: string; currentProgress: number; totalProgress: number }
+> = async ({ image, tag }, channel) => {
+  const promiseStream = (stream: any) =>
+    new Promise((resolve, reject) => {
+      stream.on('data', (data: any) => {
+        try {
+          const statusData = data.toString();
+          const status = JSON.parse(statusData);
+          channel.notify({
+            status: status.status,
+            currentProgress: status.progressDetail ? status.progressDetail.current : 0,
+            totalProgress: status.progressDetail ? status.progressDetail.total : 0,
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      });
+      stream.on('end', resolve);
+      stream.on('error', reject);
+    });
+
+  docker.image
+    .create({}, { fromImage: image, tag })
+    .then((stream: any) => promiseStream(stream))
+    .then(() => channel.reply())
+    .catch((e: Error) => channel.error(e));
 };
 
 export default {
   'docker:connect': connect,
-  'docker:pull-image': pullImage,
+  'docker:inspect': inspect,
+  'docker:pull': pull,
 };
