3c0838694813fc8d0aa0f56d14fc659b806ec72e
onsel.akin <onsela@outlook.com>
2022-03-08 23:38:57 +0300

    Added content props to scenario type

--- src/types/scenario.ts
+++ src/types/scenario.ts
@@ -3,8 +3,10 @@ import Step from './step';
 export default interface Scenario {
   id: string;
   title: string;
-
   description: string;
+
+  startContent: string;
+  finishContent: string;
 
   steps: Step[];
 }
