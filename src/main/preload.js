fad412902e9644a7a0b7c190ebca50398c27d1ea
onsel.akin <onsela@outlook.com>
2022-03-11 16:20:13 +0300

    Added logging calls for channel communications

--- src/main/preload.js
+++ src/main/preload.js
@@ -4,10 +4,12 @@ contextBridge.exposeInMainWorld('electron', {
   ipc: {
     on(channel, handler) {
       ipcRenderer.on(channel, (_event, ...args) => {
+        console.log(`[<- ${channel} (${args[0]})]: ${JSON.stringify(args[1])}`);
         handler(args);
       });
     },
     send(channel, ...args) {
+      console.log(`[-> ${channel} (${args[0]})]: ${JSON.stringify(args[1])}`);
       ipcRenderer.send(channel, args);
     },
   },
