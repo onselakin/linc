d3f7eff205450e1b7f75e9c489b45453c5759e0b
onsel.akin <onsela@outlook.com>
2022-03-20 22:40:48 +0300

    Added Scenario list to scenario information screen

--- src/renderer/screens/Labs/ScenarioInformation.tsx
+++ src/renderer/screens/Labs/ScenarioInformation.tsx
@@ -1,24 +1,42 @@
+import { InvokeChannel } from 'ipc';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
 import 'renderer/App.css';
+import progressAtom from 'renderer/atoms/progress';
 import Markdown from 'renderer/components/Markdown';
 import StepNavigation from 'renderer/components/StepNavigation';
 import { useCurrentLab, useCurrentScenario } from 'renderer/hooks/useCurrent';
+import ScenarioList from './ScenarioList';
 
 const ScenarioInformation = () => {
   const lab = useCurrentLab();
   const scenario = useCurrentScenario();
+  const [progressRecords, setProgressRecords] = useRecoilState(progressAtom);
+
+  useEffect(() => {
+    const init = async () => {
+      setProgressRecords(await InvokeChannel('progress:load', { labId: lab.id }));
+    };
+    init();
+  }, [lab, setProgressRecords]);
 
   return (
-    <>
-      <div className="prose max-w-none">
-        <Markdown markdown={scenario.startContent} />
+    <div className="flex my-4">
+      <div className="w-80">
+        <ScenarioList lab={lab} progressRecords={progressRecords} />
       </div>
-      <StepNavigation
-        nextVisible
-        nextTitle={scenario.steps[0].title}
-        previous=""
-        next={`/lab/${lab.id}/scenario/${lab.scenarios[0].id}/step/${lab.scenarios[0].steps[0].id}`}
-      />
-    </>
+      <div className="flex-1 mx-4">
+        <div className="prose max-w-none">
+          <Markdown markdown={scenario.startContent} />
+        </div>
+        <StepNavigation
+          nextVisible
+          nextTitle={scenario.steps[0].title}
+          previous=""
+          next={`/lab/${lab.id}/scenario/${lab.scenarios[0].id}/step/${lab.scenarios[0].steps[0].id}`}
+        />
+      </div>
+    </div>
   );
 };
 export default ScenarioInformation;
--- src/renderer/screens/Labs/ScenarioList.tsx
+++ src/renderer/screens/Labs/ScenarioList.tsx
@@ -6,27 +6,10 @@ import Scenario from '../../../types/scenario';
 
 type ScenarioListProps = {
   lab: Lab;
-  drawerMode: boolean;
-  needsImagePull: boolean;
-  dockerEngineUnavailable: boolean;
-  disabled: boolean;
   progressRecords: ProgressRecord[];
-  onStartLabClick: () => void;
-  onPullImageClick: () => void;
 };
 
-const ScenarioList = ({
-  lab,
-  drawerMode,
-  needsImagePull,
-  dockerEngineUnavailable,
-  disabled,
-  progressRecords,
-  onStartLabClick,
-  onPullImageClick,
-}: ScenarioListProps) => {
-  console.log('call scenariolist');
-
+const ScenarioList = ({ lab, progressRecords }: ScenarioListProps) => {
   const isScenarioCompleted = (scenario: Scenario) =>
     (scenario.steps ? scenario.steps.length : 1) ===
     progressRecords.filter(r => r.labId === lab.id && r.scenarioId === scenario.id).length;
@@ -36,73 +19,41 @@ const ScenarioList = ({
     (scenario.steps ? scenario.steps.length : 1);
 
   return (
-    <div className={`${drawerMode ? 'bg-container' : ''} `}>
-      <div className="text-white flex flex-col">
-        <div className="h-36 overflow-hidden grid place-content-center rounded mb-2">
-          <img src={lab.coverImage} alt="" />
-        </div>
-        <div>
-          <i className="fa-solid fa-flask fa-sm text-[#FFB543] w-5" />
-          {lab.title}
-        </div>
-        <div className="ml-5 text-xs">
-          <span className="text-gray-400">by </span>
-          <span>{lab.author}</span>
-        </div>
-        {!drawerMode && !needsImagePull && !dockerEngineUnavailable && (
-          <button
-            type="button"
-            className="my-4 rounded border-2 border-orange text-orange text-sm py-1 px-4 no-underline text-center"
-            onClick={onStartLabClick}
-          >
-            START LAB
-          </button>
-        )}
-        {!drawerMode && needsImagePull && !dockerEngineUnavailable && (
-          <button
-            type="button"
-            disabled={disabled}
-            className={`my-4 rounded border-2 ${
-              disabled ? 'border-gray-600 text-gray-600' : 'border-orange text-orange'
-            } text-sm py-1 px-4 no-underline text-center`}
-            onClick={onPullImageClick}
-          >
-            PULL LAB IMAGE
-          </button>
-        )}
-        {dockerEngineUnavailable && (
-          <div className="my-4 rounded border-2 border-red-400 text-red-400 text-sm py-1 px-2 no-underline text-center">
-            DOCKER ENGINE UNAVAILABLE
-          </div>
-        )}
-        {lab.scenarios.map(scenario => (
-          <div className="flex flex-col p-5 pt-3 mb-2 rounded bg-container" key={scenario.id}>
-            <div className="flex flex-row items-center text-md">
-              <div className="flex-1 text-[#5186EF]">
-                <Link to={`/lab/${lab.id}/scenario/${scenario.id}`}>{scenario.title}</Link>
-              </div>
-              <div>
-                <i
-                  className={`fa-solid fa-circle-check fa-sm self-end ${
-                    isScenarioCompleted(scenario) ? 'text-green-400' : 'text-gray-400'
-                  }`}
-                />
-              </div>
-            </div>
-            <div className="text-xs text-gray-400 mt-1">{completionPercentage(scenario)}% complete</div>
-            <div className="w-full mt-1 relative">
-              <div className="absolute bg-green h-1" style={{ width: `${completionPercentage(scenario)}%` }} />
-            </div>
-            <div className="mt-3">
-              {scenario.steps.map(step => (
-                <div key={step.id} className="mt-2 w-full flex flex-col">
-                  <div className="text-sm text-gray-300">{step.title}</div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
+    <div className="text-white flex flex-col">
+      <div className="h-36 overflow-hidden grid place-content-center rounded">
+        <img src={lab.coverImage} alt="" />
       </div>
+      <div className="my-3">
+        <i className="fa-solid fa-flask fa-sm text-[#FFB543] w-5" />
+        {lab.title}
+      </div>
+      {lab.scenarios.map(scenario => (
+        <div className="flex flex-col p-5 pt-3 mb-2 rounded bg-container" key={scenario.id}>
+          <div className="flex flex-row items-center text-md">
+            <div className="flex-1 text-[#5186EF]">
+              <Link to={`/lab/${lab.id}/scenario/${scenario.id}`}>{scenario.title}</Link>
+            </div>
+            <div>
+              <i
+                className={`fa-solid fa-circle-check fa-sm self-end ${
+                  isScenarioCompleted(scenario) ? 'text-green-400' : 'text-gray-400'
+                }`}
+              />
+            </div>
+          </div>
+          <div className="text-xs text-gray-400 mt-1">{completionPercentage(scenario)}% complete</div>
+          <div className="w-full mt-1 relative">
+            <div className="absolute bg-green h-1" style={{ width: `${completionPercentage(scenario)}%` }} />
+          </div>
+          <div className="mt-3">
+            {scenario.steps.map(step => (
+              <div key={step.id} className="mt-2 w-full flex flex-col">
+                <div className="text-sm text-gray-300">{step.title}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
