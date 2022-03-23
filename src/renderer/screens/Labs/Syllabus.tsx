4f6ae210e69af3c1a5c81d2a9d79f6c53ab6a0cd
onsel.akin <onsela@outlook.com>
2022-03-23 20:28:36 +0300

    Updated syllabus to match lab progress

--- src/renderer/screens/Labs/Syllabus.tsx
+++ src/renderer/screens/Labs/Syllabus.tsx
@@ -1,16 +1,23 @@
 import Lab from 'types/lab';
+import ProgressRecord from 'types/progressRecord';
 
 type SyllabusProps = {
   lab: Lab;
+  progressRecords: ProgressRecord[];
 };
 
-const Syllabus = ({ lab }: SyllabusProps) => {
+const Syllabus = ({ lab, progressRecords }: SyllabusProps) => {
   return (
     <div>
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
         {lab.scenarios.map(s => (
           <li className="mb-10 ml-4" key={s.id}>
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+            {progressRecords.some(p => p.scenarioId === s.id) ? (
+              <div className="absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-gray-900 bg-purple-500" />
+            ) : (
+              <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white border-gray-900 bg-gray-700" />
+            )}
+
             <h4 className="text-md font-semibold text-gray-900 dark:text-white">{s.title}</h4>
             <p className="mt-2 text-base font-normal text-gray-500 dark:text-gray-400">{s.description}</p>
             {s.estimatedTime && (
